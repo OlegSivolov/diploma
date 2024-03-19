a530cc482e215c523d5e7b088d2b6725c8839c8d
OlegSivolov <oleg.sivolov@asist-lab.com>
2024-03-19 19:18:50 +0300

    Add folder static

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    with open('index.html', 'r') as file:
-        return file.read()
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
